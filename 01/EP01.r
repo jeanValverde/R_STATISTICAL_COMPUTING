ba8ea4a0896770d25671eaffff4a3271bc22361a
Jean Carlos González Valverde <jean.gonzalez.v@usach.cl>
2023-04-03 13:52:45 -0400

    Create EP01.r

---
+++ 01/EP01.r
@@ -0,0 +1,8 @@
+#Descargar el archivo (EP01-Datos.csv) obtenido desde el repositorio del Ministerio de Ciencia, Tecnología, Conocimiento, e Innovación que da cuenta del número de casos nuevos de COVID-19 confirmados por día según resultado del diagnóstico y que han presentado síntomas, por región de residencia, reportados por el Ministerio de Salud. Esta serie de tiempo incluye los casos nuevos denominados "con síntomas" por la autoridad sanitaria.
+
+#Filtrar los datos considerando la pregunta asignada al equipo, de manera de quedarse con los datos relevantes.
+
+#Obtener la media y desviación estándar del conjunto de datos relevantes.
+
+#Seleccionar una muestra aleatoria (pero reproducible) de 16 valores. Obtenga la media de estos valores.
+

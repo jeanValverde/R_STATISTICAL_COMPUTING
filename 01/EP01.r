c810e7ae647a960d84b57dcaefbaefc01a2e54cd
Jean Carlos González Valverde <jean.gonzalez.v@usach.cl>
2023-04-03 17:48:10 -0400

    Update EP01.r

--- 01/EP01.r
+++ 01/EP01.r
@@ -6,3 +6,50 @@
 
 #Seleccionar una muestra aleatoria (pero reproducible) de 16 valores. Obtenga la media de estos valores.
 
+
+
+if(!require(ggpubr)){
+  install.packages("ggpubr", dependencies=TRUE )
+  require(ggpubr)
+}
+ 
+if(!require(ggplot2)){
+  install.packages("ggplot2", dependencies=TRUE )
+  require(ggplot2)
+}
+
+if(!require(dplyr)){
+  install.packages("dplyr", dependencies=TRUE )
+  require(dplyr)
+}
+
+if(!require(tidyverse)){
+  install.packages("tidyverse", dependencies=TRUE )
+  require(tidyverse)
+}
+
+library(ggpubr) 
+require(ggplot2)  
+library(tidyverse)
+
+
+data <- read.csv("./Datos-EP01.csv")
+
+dt <- data %>% filter(Region == "O’Higgins") 
+dt <- dt %>% select( X2022.12.21 :   X2023.03.20  )  
+
+arr <- as.matrix(dt)
+ 
+#MEDIA   
+media_pob <- mean(arr) # media de un arreglo
+media_pob
+
+#DESVIACION ESTANDAR 
+desviacion_estandar <- sd(arr)
+desviacion_estandar
+
+#MUESTRA
+tamaño_muestra <- 16
+muestra <- sample(dt, tamaño_muestra) # obtencion de muestra
+muestra
+
